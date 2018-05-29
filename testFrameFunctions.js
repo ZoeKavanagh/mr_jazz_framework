6c6d26b3872cc9a30da85c3b659535185163c3c6
Zoe Kavanagh <zoekavanagh2017@hotmail.com>
2018-05-29 17:21:15 +0100

    added framework basic testing functions, it function and html framework

---
+++ testFrameFunctions.js
@@ -0,0 +1,62 @@
+function Expect(value){
+  this.value = value
+}
+
+function expect(argument1) {
+  return (new Expect(argument1));
+}
+
+Expect.prototype.toEqual = function(secondValue){
+  if (this.value === secondValue) {
+    console.log("PASSED")
+  } else {
+    console.log("FAILED")
+  }
+};
+
+Expect.prototype.toInclude = function(value){
+  var status;
+  for (var i = 0; i < this.value.length; i++) {
+    if (this.value[i] === value) {
+      status = true;
+    }
+  }
+  if (status === true) {
+    console.log('Yes, it is in the array.');
+  } else {
+    console.log('No, it is not in the array.')
+  }
+};
+
+Expect.prototype.lengthToBe = function(value){
+  if (this.value.length === value) {
+    console.log('Yes, it is the correct length.')
+  } else {
+    console.log('No, it is the wrong length.')
+  }
+};
+
+Expect.prototype.returns = function(value){
+  if (this.value() === value) {
+    console.log('Yes, it returned the value')
+  } else {
+    console.log('No, it did not return the value')
+  }
+};
+
+Expect.prototype.throwsError = function(value){
+  try {
+    this.value();
+  }
+  catch(e) {
+    if (e === value) {
+      console.log('This is the correct error')
+    }
+  }
+}
+
+// function errorThrower(){
+//   throw "this is an error";
+// }
+//
+// expect(function(){throw 'error'}).throwsError('error');
