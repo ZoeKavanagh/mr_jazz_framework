c5e885fbb60adc94cccb5fef0dcfe4cf8ea0e83b
anderscodes <anders.j.persson@hotmail.com>
2018-05-31 17:00:27 +0100

    more double test

--- fatherJazz/doubleSpec.js
+++ fatherJazz/doubleSpec.js
@@ -5,5 +5,7 @@ testDouble = stuntDouble('double', ['length'], ['true'])
 console.log('Equal if number 1 is in the array')
 console.log(expect(testDouble.name).toEqual('double'))
 
+console.log('Equal if number 1 is in the array')
+console.log(expect(testDouble).toEqual( {name: 'double', length: 'true' } ))
 
-console.log(testDouble.name)
+console.log(testDouble)
