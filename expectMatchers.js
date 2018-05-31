238fe336deb873c168dc555130fa935551d8b646
anderscodes <anders.j.persson@hotmail.com>
2018-05-31 11:14:58 +0100

    toinclude

--- expectMatchers.js
+++ expectMatchers.js
@@ -20,7 +20,8 @@ Expect.prototype.toEqual = function(value2){
 Expect.prototype.toInclude = function(value){
   var status;
   for (var i = 0; i < this.value.length; i++) {
-    if (this.value[i] === value) {
+    if (typeof this.value[i] === typeof value) {
+      if (String(this.value[i]) === String(value))
       status = true;
     }
   }
--- fatherJazz/includeSpec.js
+++ fatherJazz/includeSpec.js
@@ -10,3 +10,19 @@ console.log(expect(expect([1,2,3]).toInclude(4)).toEqual('No, it is not in the a
 
 console.log('Passed if it is a empty array')
 console.log(expect(expect([]).toInclude('')).toEqual('No, it is not in the array.'))
+
+console.log('Passed if an array is inside the array')
+console.log(expect(expect([[1],[2],[3]]).toInclude([3])).toEqual('Yes, it is in the array.'))
+
+console.log('Passed if an array is not in the array')
+console.log(expect(expect([[1],[2],[3]]).toInclude([35])).toEqual('No, it is not in the array.'))
+
+console.log('Passed if an array is inside the array')
+console.log(expect(expect([[1],[2],[3]]).toInclude([3])).toEqual('Yes, it is in the array.'))
+
+
+obj = new Object
+obj2 = new Object
+
+console.log('Passed if an array is inside the array')
+console.log(expect(expect([obj]).toInclude(obj2)).toEqual('No, it is not in the array.'))
